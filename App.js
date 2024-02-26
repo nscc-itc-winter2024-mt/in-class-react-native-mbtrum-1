9f2725ed1543d9ffa47865a37397254b829f555d
Michael Trumbull <98288443+mbtrum@users.noreply.github.com>
2024-02-26 14:31:54 +0000

    UI Updates and submit post to wordpress.

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { StyleSheet, View, Pressable, Text, Image, TextInput, ActivityIndicator, ScrollView } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import React, { useState } from 'react';
 import {NavigationContainer, StackActions} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import base64 from 'react-native-base64';
 
 const Stack = createNativeStackNavigator();
+const host = 'https://nscc-0304263-wordpress-photos.azurewebsites.net';
+const username = '';
+const password = ''; // api token
+
 
 function HomeScreen( { navigation } ){
   const [image, setImage] = useState(null);
+  const [title, setTitle] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   // Function to choose photo on phone
   const pickImage = async () => {
@@ -26,15 +33,103 @@ function HomeScreen( { navigation } ){
 
       setImage(uri);
     }
-
   }
 
+  // Upload Featured Image to WordPress
+  const uploadPhoto = async () => {
+    const endPoint = host + '/wp-json/wp/v2/media';
+    const fileName = image.split('/').pop();
+    const formData = new FormData();
+
+    formData.append('file', { 
+      uri: image,
+      name: fileName
+    });
+
+    const result = await fetch(endPoint, {
+      method: 'POST',
+      headers: {        
+        'Content-disposition': 'formdata; filename=' + fileName,
+        'Authorization': 'Basic ' + base64.encode(username + ':' + password)
+      },
+      body: formData
+    });
+
+    const response = await result.json();
+    const mediaId = response.id;
+    
+    return mediaId;
+  }
+
+  // Create Post in WordPress
+  const submitPost = async () => {
+    
+    // Validate inputs
+    if(!title || !image){
+      alert('Please complete all input fields.');
+    }
+    else {
+
+      setIsLoading(true);
+
+      // Create media Api
+      const mediaId = await uploadPhoto();
+      console.log('Media ID: ' + mediaId);
+
+      // Create Post Api
+      const endPoint = host + '/wp-json/wp/v2/posts';
+      
+      const formData = new FormData();
+      formData.append('title', title);
+      formData.append('status', 'publish');
+      formData.append('featured_media', mediaId);
+
+      // Create the post
+      const result = await fetch(endPoint, {
+        method: 'POST',
+        headers: {
+          'Authorization': 'Basic ' + base64.encode(username + ':' + password)
+        },
+        body: formData
+      });
+
+      const response = await result.json();
+
+      if(response.id) {
+        alert('Successfully created Post. ID: ' + response.id);        
+      }
+      else {
+        alert('Opps, something went wrong.');
+      }
+
+      setIsLoading(false);      
+    }
+  }
+
+  
+
   return (
-    <View style={styles.container}>
-      <Button title="Choose an Image" onPress={ pickImage }></Button>
-      { image && <Image source={{ uri: image }} style={{ width:250, height: 250, resizeMode: 'cover' }} /> }
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <TextInput 
+          style={ styles.input } 
+          placeholder="Title"
+          onChangeText={ text => setTitle(text) } 
+          defaultValue= { title } />
+
+        <Pressable onPress={ pickImage } style={ styles.button }>
+          <Text style={ styles.buttonText }>Choose Image</Text>
+        </Pressable>
+
+        { image && <Image source={{ uri: image }} style={{ width:250, height: 250, resizeMode: 'cover' }} /> }
+
+        <Pressable onPress={ submitPost }  style={ styles.button }>
+          <Text style={ styles.buttonText }>Submit</Text>
+        </Pressable>
+        
+        { isLoading && <ActivityIndicator /> }
+      </View>
+    </ScrollView>
   );
 }
 
@@ -51,8 +146,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    alignItems: 'center',
+    marginTop: 50
+    
+  },
+  input: {
+    height: 40,
+    marginBottom: 10,
+    borderWidth: 1,
+    padding: 10,
+    width: 300,
+  },
+  button: {
     alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'slateblue',
+    marginBottom: 10,
   },
+  buttonText: {
+    fontSize: 16,
+    lineHeight: 21,
+    letterSpacing: 0.25,
+    color: 'white',
+  }
 });
